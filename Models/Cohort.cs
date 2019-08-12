f9114bc57dd912cc879b5ac057083b15f2bb69f5
Carly Bergthold <46655107+carlybergthold@users.noreply.github.com>
2019-08-12 12:49:43 -0700

    first commit

---
+++ Models/Cohort.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentExercisesMVC.Models
+{
+    public class Cohort
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string CohortName { get; set; }
+        public List<Student> Students { get; set; }
+        public List<Instructor> Instructors { get; set; }
+    }
+}
